b5a3b9cc36d991b03dae414b68784c5b4c3005dd
2SeungChan <105332962+LSC18@users.noreply.github.com>
2023-01-30 00:22:21 +0900

    [Gold V] Title: 로봇 청소기, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -1,44 +1,39 @@
-import sys
-input = sys.stdin.readline
-from collections import deque
+N, M = map(int, input().split())
+x, y, d = map(int, input().split())
 
-n,m = map(int,input().split())
-graph = []
-visited = [[0] * m for _ in range(n)]
-r,c,d = map(int,input().split())
+visited = [[0] * M for _ in range(N)]
 
-# d => 0,3,2,1 순서로 돌아야한다.
-dx = [-1,0,1,0]
+dx = [-1,0,1,0] 
+# 북(0)쪽을 바라볼 때 왼쪽의 x좌표값은 -1, 동(1)쪽을 바라볼 때 왼쪽의 x좌표값은 0...
 dy = [0,1,0,-1]
+# 북(0)쪽을 바라볼 때 왼쪽의 y좌표값은 0, 동(1)쪽을 바라볼 때 왼쪽의 y좌표값은 1...
 
-for _ in range(n):
-    graph.append(list(map(int,input().split())))
+field = [list(map(int, input().split())) for _ in range(N)]
 
-# 처음 시작하는 곳 방문 처리
-visited[r][c] = 1
+visited[x][y] = 1
 cnt = 1
 
-while 1:
+while True:
     flag = 0
-    # 4방향 확인
-    for _ in range(4):
-        # 0,3,2,1 순서 만들어주기위한 식
-        nx = r + dx[(d+3)%4]
-        ny = c + dy[(d+3)%4]
-        # 한번 돌았으면 그 방향으로 작업시작
-        d = (d+3)%4
-        if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 0:
+    for i in range(4):
+        d = (d + 3) % 4
+        # direction = 3 - direction 보다 명확한 식
+        #왼쪽으로 한칸 돌리면 0,1,2,3 -> 3,2,1,0이 됨.
+        nx = x + dx[d]
+        ny = y + dy[d]
+
+        if 0 <= nx < N and 0 <= ny < M and field[nx][ny] == 0:
             if visited[nx][ny] == 0:
                 visited[nx][ny] = 1
+                x = nx
+                y = ny
                 cnt += 1
-                r = nx
-                c = ny
-                #청소 한 방향이라도 했으면 다음으로 넘어감
                 flag = 1
                 break
-    if flag == 0: # 4방향 모두 청소가 되어 있을 때,
-        if graph[r-dx[d]][c-dy[d]] == 1: #후진했는데 벽
+    if flag == 0: # 사방이 청소 불가일 떄
+        if field[x-dx[d]][y-dy[d]] == 1:
             print(cnt)
             break
         else:
-            r,c = r-dx[d],c-dy[d]
+            x = x-dx[d]
+            y = y-dy[d]
