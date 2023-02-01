55fc0b3febba978b758dccba124ccc257338ceee
2SeungChan <105332962+LSC18@users.noreply.github.com>
2023-02-01 19:55:20 +0900

    [Gold IV] Title: PPAP, Time: 304 ms, Memory: 38308 KB -BaekjoonHub

--- 백준/Gold/16120. PPAP/PPAP.py
+++ 백준/Gold/16120. PPAP/PPAP.py
@@ -1,14 +1,9 @@
 N = input()
-PA = []
-
-for i in range(len(N)):
-    PA.append(N[i])
 
 stk = []
-cnt = 0
-while cnt < len(N):
-    stk.append(PA[cnt])
-    cnt += 1
+
+for i in N:
+    stk.append(i)
     if len(stk) >= 4:
         if stk[-1] == 'P' and stk[-2] == 'A' and stk[-3] == 'P' and stk[-4] == 'P':
             stk.pop()
