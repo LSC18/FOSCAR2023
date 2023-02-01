86e24e27da8b648f9b01d2de2765e5c541ab2834
2SeungChan <105332962+LSC18@users.noreply.github.com>
2023-02-01 22:30:50 +0900

    [Silver I] Title: 팔, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1105. 팔/팔.py
@@ -0,0 +1,14 @@
+L, R = map(str, input().split())
+
+ans = 0
+
+if len(L) != len(R):
+    print(0)
+else:
+    for i in range(len(L)):
+        if L[i] == R[i]:
+            if L[i] == '8':
+                ans += 1
+        else:
+            break
+    print(ans)
